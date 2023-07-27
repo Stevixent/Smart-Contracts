61468effe27c4cd82d83797b40095ed1c682854a
Stevixent <chiadikavirtue@gmail.com>
2023-07-27 15:12:04 +0100

    push

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -2,5 +2,16 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 import "./SimpleStorage.sol";
+
 contract StorageFactory {
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        
+    }
 }
